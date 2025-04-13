c44b2b78ee5effdcc2befec03dd91e690f495cfe
jinethmopada <jinethmopada@gmail.com>
2025-04-13 12:00:12 +0530

    css added for resto cards

--- src/Components/RestaurantCard.jsx
+++ src/Components/RestaurantCard.jsx
@@ -4,11 +4,11 @@ const Card = ({cloudinaryImageId,name,costForTwo,cuisines,avgRating,locality}) =
         return(
             <div className="restaurant-card">
                 <img className="rest-image" src={CommonImageUrl + cloudinaryImageId} alt="logo"/>
-                <h1>{name}</h1>
-                <h2>{costForTwo}</h2>
-                <h3>{locality}</h3>
-                <h4>{cuisines.join(', ')}</h4>
-                <p>{avgRating}</p>
+                <h1 className="resto-name">{name}</h1>
+                <h2 className="resto-name">{costForTwo}</h2>
+                <h3 className="resto-name">{locality}</h3>
+                <h4 className="resto-name">{cuisines.join(', ')}</h4>
+                <p className="resto-name">{avgRating}</p>
             </div>
         )
     }
